e4be68623f6ed66be1db8cf9ab8b7ba8c11e4485
FÁBIO LUÍS GOMES GONÇALVES DE OLIVEIRA <fabio_luis_oliveira@yahoo.com.br>
2023-02-01 14:14:07 -0500

    Projet créé

---
+++ src/main/java/com/fabio/projetmongo/ProjetmongoApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.projetmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio/projetmongo/ProjetmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.projetmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
